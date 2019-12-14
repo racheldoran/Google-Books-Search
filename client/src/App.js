940ffe7d73fcadc460db1fea17d047ca4b8545e8
rachel doran <rmitchell0925@gmail.com>
2019-12-14 13:32:57 -0500

    lord help me

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/Jumbotron/header";
-
+import NavBar from "./components/NavBar";
+import Jumbotron from "./components/Jumbotron";
+import Wrapper from "./components/Wrapper";
 
 import NoMatch from "./pages/NoMatch";
 import Search from "./pages/search";
@@ -14,13 +15,14 @@ class App extends Component {
     return (
       <Router>
         <div>
-         <NavBar />
-          <Header />
+          <NavBar />
+          <Jumbotron />
+          <Wrapper>
             <Route exact path="/" component={Search} />
             <Route exact path="/search" component={Search} />
             <Route exact path="/saved" component={Saved} />
             <Route exact path="/noMatch" component={NoMatch} />
-          
+          </Wrapper>
         </div>
       </Router>
     )
